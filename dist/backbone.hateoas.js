4459bd6aba527aa499b590005791a68c5959ed82
Alik Kurdyukov <alik@kurdyukov.com>
2015-06-18 11:09:37 +0300

    server collection paging improved

--- dist/backbone.hateoas.js
+++ dist/backbone.hateoas.js
@@ -1228,7 +1228,7 @@
           }
     
         /**
-         * Specialized Hal Collection.
+         * Specialized Hal Collection. User should set `rel` property.
          * 
          * @author Baptiste GAILLARD (baptiste.gaillard@gomoob.com)
          * @author Simon BAUDRY (simon.baudry@gomoob.com)
@@ -1251,6 +1251,14 @@
                     firstPage : 1,
                     pageSize : 12
                 },
+                /**
+                   @property {string} name of relation in the collection. Usually it's the only key in _embedded collection
+                */
+                rel: null,
+                /**
+                   @property {string} name if 'page' entity for current paging position
+                */
+                pageProperty: null,
                 
                 parseLinks: function (resp, xhr) {
                     
@@ -1278,7 +1286,8 @@
     
                     var links = {};
                     
-                    if(resp.total_items !== 0) {
+                    var page = this._getPageEntity(resp);
+                    if(page[queryParams.totalRecords] !== 0) {
                         links.first = resp._links.first.href;
                         links.last = resp._links.last.href;
                     }
@@ -1299,21 +1308,36 @@
     
                     // The 'rel' parameter is required !
                     if(!this.rel) {
-    
                         throw new Error('A \'rel\' parameter is required !');
-    
                     }
     
                     return resp._embedded[this.rel];
-                        
+                },
+    
+                _getPageEntity: function(resp) {
+                    if (this.pageProperty && _.has(resp, this.pageProperty)) {
+                        return resp[this.pageProperty];
+                    } else {
+                        return resp;
+                    }
                 },
     
                 parseState: function (resp, queryParams, state, options) {
-                    
-                    return {
-                        totalItems: resp.total_items
+                    var page = this._getPageEntity(resp);
+    
+                    var newState = {
+                        currentPage: page[queryParams.currentPage],
+                        pageSize: page[queryParams.pageSize]
                     };
-                    
+    
+                    if (queryParams.totalRecords && _.has(page, queryParams.totalRecords)) {
+                        newState.totalRecords = page[queryParams.totalRecords];
+                    }
+                    if (queryParams.totalPages && _.has(page, queryParams.totalPages)) {
+                        newState.totalPages = page[queryParams.totalPages];
+                    }
+    
+                    return newState;
                 },
                 
                 // FIXME: Cette fonction a presque le même code que PageableCollection.getPage(index, options) excepté 
--- test/spec/collection.js
+++ test/spec/collection.js
@@ -1,7 +1,7 @@
 /*jshint -W030 */
 
 describe(
-    'Hal.Collection', 
+    'Hal.Collection',
     function() {
 
         describe('initialize', function() {
@@ -10,6 +10,111 @@ describe(
 
                 var collection = new Hal.Collection();
 
+            });
+
+        });
+
+        describe('parse', function() {
+            it('With server mode', function() {
+                var CustomCollection = Hal.Collection.extend({
+                    queryParams: {
+                        currentPage: 'page',
+                        pageSize: 'page_size',
+                        totalPages: 'totalPages',
+                        totalRecords: 'totalElements'
+                    },
+                    mode: 'server'
+                });
+                var collection = new CustomCollection();
+                collection.rel = 'users';
+                collection.parse({
+                    '_links': {
+                        'self': {
+                            'href': 'http://localhost:8080/rest/users{?page,size,sort}',
+                            'templated': true
+                        },
+                        'search': {
+                            'href': 'http://localhost:8080/rest/users/search'
+                        }
+                    },
+                    '_embedded': {
+                        'users': [{
+                            'firstName': 'Baptiste',
+                            'lastName': 'Gaillard'
+                        }, {
+                            'firstName': 'Simon',
+                            'lastName': 'Baudry'
+                        }, {
+                            'firstName': 'John',
+                            'lastName': 'Doe',
+                        }]
+                    },
+                    'page_size': 20,
+                    'totalElements': 3,
+                    'totalPages': 1,
+                    'page': 1
+                });
+
+                // assert
+                expect(collection.state.currentPage).to.equal(1);
+                expect(collection.state.totalRecords).to.equal(3);
+                expect(collection.state.pageSize).to.equal(20);
+                expect(collection.hasNextPage()).to.equal(false);
+                expect(collection.hasPreviousPage()).to.equal(false);
+            });
+
+            it('With server mode and custom paging', function() {
+                var CustomCollection = Hal.Collection.extend({
+                    queryParams: {
+                        currentPage: 'number',
+                        pageSize: 'size',
+                        totalPages: 'totalPages',
+                        totalRecords: 'totalElements'
+                    },
+                    state: {
+                        firstPage: 0
+                    },
+                    pageProperty: 'page',
+                    mode: 'server'
+                });
+                var collection = new CustomCollection();
+                collection.rel = 'users';
+                collection.parse({
+                    '_links': {
+                        'self': {
+                            'href': 'http://localhost:8080/rest/users{?page,size,sort}',
+                            'templated': true
+                        },
+                        'search': {
+                            'href': 'http://localhost:8080/rest/users/search'
+                        }
+                    },
+                    '_embedded': {
+                        'users': [{
+                            'firstName': 'Baptiste',
+                            'lastName': 'Gaillard'
+                        }, {
+                            'firstName': 'Simon',
+                            'lastName': 'Baudry'
+                        }, {
+                            'firstName': 'John',
+                            'lastName': 'Doe',
+                        }]
+                    },
+                    'page': {
+                        'size': 20,
+                        'totalElements': 3,
+                        'totalPages': 1,
+                        'number': 0
+                    }
+                });
+
+                // assert
+                expect(collection.state.currentPage).to.equal(0);
+                expect(collection.state.totalRecords).to.equal(3);
+                expect(collection.state.pageSize).to.equal(20);
+                expect(collection.hasNextPage()).to.equal(false);
+                expect(collection.hasPreviousPage()).to.equal(false);
             });
 
         });
